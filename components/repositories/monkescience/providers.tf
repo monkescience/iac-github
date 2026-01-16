ceb3c8c601cfd617713138e4501f1cb622b1ef42
ValentinLutz <allround.email@gmail.com>
2026-01-17 00:18:04 +0100

    refactor(repositories): consolidate terraform config into providers.tf

--- components/repositories/monkescience/providers.tf
+++ components/repositories/monkescience/providers.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = ">= 1.11"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.9.1"
+    }
+  }
+
+  backend "local" {}
+}
+
 provider "github" {
   owner = var.owner
 }
